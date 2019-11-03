3bb2af046e404c57aeabfafb55ad6951e317bba5
Tetsuya Taguchi <bouzu.ao@gmail.com>
2019-11-03 21:37:08 +0900

    add refresh alert

--- assets/javascripts/timer.js
+++ assets/javascripts/timer.js
@@ -68,3 +68,8 @@ var resetTimer = function (resetType) {
   displayNumber(formatTime())
 }
 displayNumber(formatTime())
+window.addEventListener("beforeunload", function (e) {
+  var confirmationMessage = "\o/";
+  (e || window.event).returnValue = confirmationMessage;
+  return confirmationMessage;
+});
